a527d3bb7551bdcbad47dec5723ed26dc5bd1222
Syuko4omi <hiromasasakurai@hiromasasakurainoMacBook-Pro.local>
2020-11-17 05:39:09 +0900

    week7

---
+++ txt_to_bin.py
@@ -0,0 +1,9 @@
+import sys
+
+with open(sys.argv[1]) as fin,  open(sys.argv[2], "wb") as fout:
+    while True:
+        s = fin.readline()
+        if s:
+            fout.write(int(s,16).to_bytes(len(s)//2,"little"))
+        else:
+            break
